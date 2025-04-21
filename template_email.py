52567820bb80fcb24298fbebc36e0124f00d3f94
davipythonweb <galaxys3tab32@gmail.com>
2025-04-21 14:02:33 -0300

    create template email - class type str

---
+++ template_email.py
@@ -0,0 +1,23 @@
+clientes = ["Ana", "Alana", "Davi"]
+
+email_template = """
+Ola, %(cliente)s 
+Tem interesse em comprar %(produto)s?
+Este produto eh otimo para resolver %(problema)s
+click aqui para comprar %(link)s
+Apenas %(quantidade)d unidades disponiveis!
+Preco promocional $%(preco).2f
+"""
+
+for cliente in clientes:
+    print(
+        email_template % {
+            'cliente': cliente,
+            'produto': 'caneta',
+            'problema': 'na hora de escrever',
+            'link': 'http://canetaslegais.com.br',
+            'quantidade': 1,
+            'preco': 40.5
+        }
+    )
+    
