96df36699a96e706b3d698489eff5c6b176675cb
davipythonweb <galaxys3tab32@gmail.com>
2025-04-21 00:55:07 -0300

    Add a root URL route that returns a simple HTML response

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: HttpResponse('<h1>Ola Mundo</h1>')),
 ]
+
+
