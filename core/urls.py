b91e2b24420e423b21539ecc03639327d69f3a1f
davipythonweb <galaxys3tab32@gmail.com>
2025-04-21 22:19:00 -0300

    Add 'home' and 'blog' apps to settings and update URL routing

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
+from home import views as home_views
+from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', lambda request: HttpResponse('<h1 style="color: indigo;font-size: 50px;font-family: Arial, Helvetica, sans-serif;text-align: center;text-decoration: underline;text-transform: uppercase;">Ola Mundo</h1>')),
+    path('', home_views.home, name='home'),
+    path('blog/', blog_views.blog, name='blog'),
+    path('teste/', lambda request: HttpResponse('<h1 style="color: indigo;font-size: 50px;font-family: Arial, Helvetica, sans-serif;text-align: center;text-decoration: underline;text-transform: uppercase;">Ola Mundo</h1>')),
 ]
---
+++ home/views.py
@@ -0,0 +1,6 @@
+# from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    print('home')
+    return HttpResponse('home do app')
